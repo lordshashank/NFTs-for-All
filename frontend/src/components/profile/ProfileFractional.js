6f45513208aea5c37e12bf2e0601c29872a48061
me-loser <puspendramahariya@gmail.com>
2023-03-30 23:26:53 +0530

    merged with the branch

--- frontend/src/components/profile/ProfileFractional.js
+++ frontend/src/components/profile/ProfileFractional.js
@@ -12,57 +12,11 @@ import { v4 } from "uuid";
 const ProfileFractional = () => {
   const router = useRouter();
   const { userAccount } = useWeb3();
-<<<<<<< HEAD
-  const { isLoading, fetchData } = useFetchData();
-  const contracts = useSelector((state) => state.deals.contracts);
-  console.log(contracts);
-  const { tokenIds } = useTokenId("deals");
-  const fractionalNfts = useSelector((state) => state.deals.fractionalData);
-  console.log(fractionalNfts);
-  useEffect(() => {
-    if (userAccount) {
-      const url = `http://localhost:8000/get-fractional-contracts/${userAccount}`;
-      fetchData(url, dealsActions.addContract);
-    }
-  }, [userAccount]);
-
-  useEffect(() => {
-    if (tokenIds !== [] && tokenIds !== null && tokenIds !== "undefined") {
-      getFractionalData();
-    }
-  }, [tokenIds]);
-
-  const getFractionalData = useCallback(async () => {
-    console.log(1.1);
-    try {
-      console.log(tokenIds);
-      const response = await fetch("http://localhost:8000/fractional-data", {
-        method: "POST",
-        body: JSON.stringify({ tokenIds: tokenIds }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const resData = await response.json();
-      console.log(resData);
-      dispatch(profilesActions.addFractionalData({ fractionalData: resData }));
-      console.log(fractionalNfts);
-      // dispatch(dealsActions.addFractionalData({ fractionalData: resData }));
-    } catch (error) {
-      console.error(error);
-    }
-  });
-  console.log(fractionalNfts);
-  if (!isLoading && fractionalNfts.length === 0) {
-    return <h1>Data Not Found</h1>;
-  }
-=======
   const fractionalNfts = useSelector((state) => state.deals.fractionalData);
   const url = `http://localhost:8000/get-fractional-contracts/${userAccount}`;
   const { isLoading: isLoadingContracts } = useFetchContract(url);
   const { isLoading: isLoadingNfts } = useFractionalNfts();
 
->>>>>>> a83f483e0440e428b632b0aad41bc44189b99c2a
   return (
     <div>
       {isLoadingContracts || isLoadingNfts ? (
